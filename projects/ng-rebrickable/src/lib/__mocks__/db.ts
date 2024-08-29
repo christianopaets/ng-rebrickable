23964c6b7bfe4f8bb0ef9b74e6ac0dbb614f6bf6
Kristian Opayets <43338174+christianopaets@users.noreply.github.com>
2024-08-29 20:02:35 +0300

    Use consistent double quotes for imports
    Updated all import statements to use double quotes instead of single quotes. This change enhances code consistency and alignment with common coding standards.

--- projects/ng-rebrickable/src/lib/__mocks__/db.ts
+++ projects/ng-rebrickable/src/lib/__mocks__/db.ts
@@ -1,9 +1,9 @@
-import * as colors from './colors.json';
-import * as color from './color.json';
-import * as themes from './themes.json';
-import * as theme from './theme-response.json';
-import * as minifigs from './minifigs.json';
-import * as minifig from './minifig.json';
+import * as colors from "./colors.json";
+import * as color from "./color.json";
+import * as themes from "./themes.json";
+import * as theme from "./theme-response.json";
+import * as minifigs from "./minifigs.json";
+import * as minifig from "./minifig.json";
 
 export namespace DbMocks {
   export const Colors = colors;
